79b6fd48b52ea946ac1ed11fd61ef893502a0884
lucabertelli <bertelli.l86@gmail.com>
2023-12-10 06:38:37 +0100

    feat: add purge of CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [purgecss()]
 });
