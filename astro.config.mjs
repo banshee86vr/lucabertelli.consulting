576dbf1b3b7fb5689eca37776fd4b7ccac0ec78b
lucabertelli <bertelli.l86@gmail.com>
2023-12-10 06:42:05 +0100

    feat: Add sentry and spotlightjs integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import purgecss from "astro-purgecss";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [purgecss()]
+  integrations: [purgecss(), sentry(), spotlightjs()]
 });
