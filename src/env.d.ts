5ff6eacc53911286fc65f656da1b3c740a898bc6
Luca Bertelli <bertelli.luca@proton.me>
2024-07-15 15:34:00 +0200

    fix: test secrets loading

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,3 +9,14 @@ declare namespace NodeJS {
     SECRET_LOAD_EVENTS: string;
   }
 }
+
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {
+  lb_consulting: KVNamespace;
+};
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+declare namespace App {
+  interface Locals extends Runtime {}
+}
